3f1997dda307e78ca82f288146d8885c013bbe45
kauanmn <kauan.manzatoo@gmail.com>
2020-12-04 18:32:02 -0300

    Add files via upload

---
+++ src/Main.hs
@@ -0,0 +1,123 @@
+module Main where
+
+import System.Directory
+import System.IO
+import System.Random
+import System.Environment
+import Data.List
+import Rsa
+
+
+
+dispatch :: [String] -> IO ()
+dispatch ("help":_)                        = do
+    putStrLn "Lista de argumentos para o programa:"
+    putStrLn "help\t\t\t\tabre o menu de ajuda"
+    putStrLn "rsa keygen\t\t\tgera um par de chaves RSA e salve em dois arquivos separados"
+    putStrLn "rsa encrypt_text _\t\tcriptografa o conteudo inserido no terminal usando o algoritmo RSA"
+    putStrLn "rsa encrypt_text filePath\tcriptografa o conteudo de um arquivo usando o algoritmo RSA"
+    putStrLn "rsa decrypt_text _\t\tdescriptografa o conteudo inserido no terminal usando o algoritmo RSA"
+    putStrLn "rsa decrypt_text filePath\tdescriptografa o conteudo de um arquivo usando o algoritmo RSA"
+dispatch ("rsa":"keygen":_)                = rsa_keygen
+
+dispatch ("rsa":"encrypt_text":_)          = rsa_encrypt_text
+dispatch ("rsa":"encrypt_file":filePath:_) = rsa_encrypt_file filePath
+
+dispatch ("rsa":"decrypt_text":_)          = rsa_decrypt_text
+dispatch ("rsa":"decrypt_file":filePath:_) = rsa_decrypt_file filePath
+
+dispatch ("rsa":_) = putStrLn "Faltam argumentos. Use o comando 'help' para saber mais."
+dispatch _ = putStrLn "Comando não reconhecido. Use o comando 'help' para saber mais."
+
+
+
+main :: IO ()
+main = do
+    argumentos <- getArgs
+    dispatch argumentos
+
+
+
+-- funcao IO() para gerar um par de chaves RSA
+rsa_keygen :: IO ()
+rsa_keygen = do
+    chavePublica  <- openFile "chaveRSA_Publica.txt" WriteMode
+    chavePrivada  <- openFile "chaveRSA_Privada.txt" WriteMode
+    randGen <- getStdGen
+    putStr "Aguarde..."
+    let (p,q) = obterPQ $ listaPrimos randGen
+    hPutStrLn chavePublica $ criarChavePublica (p,q)
+    hPutStrLn chavePrivada $ criarChavePrivada (p,q)
+    putStr "Pronto. Suas chaves foram salvas em dois arquivos diferentes.\n"
+    putStr "Chave pública: chaveRSA_Publica.txt\n"
+    putStr "Chave privada: chaveRSA_Privada.txt\n"
+    hClose chavePublica
+    hClose chavePrivada
+
+
+
+-- funcao IO() para descriptografia RSA de um texto
+rsa_decrypt_text :: IO ()
+rsa_decrypt_text = do
+    chavePrivada <- openFile "chaveRSA_Privada.txt" ReadMode
+    chave    <- hGetLine chavePrivada
+    putStrLn "Insira o conteudo a ser descriptografado:"
+    conteudo <- getLine
+    let (n,d) = read (chave) :: (Integer, Integer)
+    let conteudoDecifrado = descriptografar conteudo (n, d)
+    putStrLn (conteudoDecifrado ++ "\n")
+    putStrLn "Nome do arquivo para salvar os dados:"
+    fileName <- getLine
+    writeFile fileName (conteudoDecifrado ++ "\n")
+    hClose chavePrivada
+
+
+
+-- funcao IO() para descriptografia RSA de um arquivo
+rsa_decrypt_file :: FilePath -> IO ()
+rsa_decrypt_file filePath = do
+    chavePrivada <- openFile "chaveRSA_Privada.txt" ReadMode
+    chave    <- hGetLine chavePrivada
+    secretTextHandle <- openFile filePath ReadMode
+    conteudo <- hGetLine secretTextHandle
+    let (n,d) = read (chave) :: (Integer, Integer)
+    let conteudoDecifrado = descriptografar conteudo (n, d)
+    putStrLn (conteudoDecifrado ++ "\n")
+    putStrLn "Nome do arquivo para salvar os dados:"
+    fileName <- getLine
+    writeFile fileName (conteudoDecifrado ++ "\n")
+    hClose chavePrivada
+
+
+
+-- funcao IO() para criptografia RSA de um texto
+rsa_encrypt_text :: IO ()
+rsa_encrypt_text = do
+    chavePublica <- openFile "chaveRSA_Publica.txt" ReadMode
+    chave    <- hGetLine chavePublica
+    putStrLn "Insira o conteudo a ser criptografado:"
+    conteudo <- getLine
+    let (n,e) = read (chave) :: (Integer, Integer)
+    let conteudoCifrado = criptografar conteudo (n,e)
+    putStrLn (conteudoCifrado ++ "\n")
+    putStrLn "Nome do arquivo para salvar os dados:"
+    fileName <- getLine
+    writeFile fileName (conteudoCifrado ++ "\n")
+    hClose chavePublica
+
+
+
+-- funcao IO() para criptografia RSA de um arquivo
+rsa_encrypt_file :: FilePath -> IO ()
+rsa_encrypt_file filePath = do
+    chavePublica <- openFile "chaveRSA_Publica.txt" ReadMode
+    chave    <- hGetLine chavePublica
+    plainTextHandle <- openFile filePath ReadMode
+    conteudo <- hGetLine plainTextHandle
+    let (n,e) = read (chave) :: (Integer, Integer)
+    let conteudoCifrado = criptografar conteudo (n,e)
+    putStrLn (conteudoCifrado ++ "\n")
+    putStrLn "Nome do arquivo para salvar os dados:"
+    fileName <- getLine
+    writeFile fileName (conteudoCifrado ++ "\n")
+    hClose chavePublica
---
+++ src/Rsa.hs
@@ -0,0 +1,192 @@
+module Rsa
+( criptografar
+, descriptografar
+, criarChavePrivada
+, criarChavePublica
+, obterPQ
+, listaPrimos ) where 
+
+import Data.Char
+import System.IO
+import System.Random
+
+
+
+-- retorna o texto criptografado codificado em uma lista de inteiros
+criptografar :: String -> (Integer, Integer) -> String
+criptografar plainText (n,e)  = show plainTextList 
+    where plainTextList = map (\x -> moduloExp x e n) $ codificarTextoInt plainText
+
+
+
+-- retorna o texto original de um texto criptografado e codificado em uma lista de inteiros
+descriptografar :: String -> (Integer,Integer) -> String
+descriptografar secretText (n,d) = decodificarIntTexto $ map (\x -> moduloExp x d n ) $ secretTextList
+    where secretTextList = read secretText :: [Integer]
+
+
+
+-- retorna a chave publica, representada por uma string (n,e)
+criarChavePublica :: (Integer, Integer) -> String
+criarChavePublica (p, q) = show (n,e)
+    where n = obterN (p,q)
+          e = 3
+
+
+
+-- retorna a chave privada, representada por uma string (n,d)
+criarChavePrivada :: (Integer, Integer) -> String
+criarChavePrivada (p,q) = show (n,d) 
+    where n = obterN (p,q)
+          d = obterD $ obterPhi (p,q)
+
+
+
+-------------------------------------------------------------------------------------
+-- FUNCOES AUXILIARES --
+
+-- retorna n
+obterN :: (Integer, Integer) -> Integer
+obterN (p, q) = p*q
+
+
+
+-- algoritmo de Euclides para achar o maximo divisor comum
+obterMDC :: (Integer,Integer) -> (Integer, Integer)
+obterMDC (a,b)
+    | (b == 0) = (1,0)
+    | otherwise = (t, z)
+    where (s,t) = obterMDC(b, a `mod` b)
+          z = s - ( (a `div` b) * t)
+
+
+
+-- retorna phi(n)
+obterPhi :: (Integer, Integer) -> Integer
+obterPhi (p, q) = (p-1)*(q-1)
+
+
+
+-- retorna d
+obterD :: Integer -> Integer
+obterD phi = if y < 0 then (y + phi) else y
+    where (_,y) = obterMDC (phi, e)
+          e = 3
+
+
+
+-- retorna uma lista de numeros entre com 512 bits de tamanho
+listaPrimos :: StdGen -> [Integer]
+listaPrimos gen = filter (primoMillerRabin gen) numAleatorios
+    where numAleatorios = randomRs ( (2^511), (2^513) ) gen
+
+
+
+-- retorna p
+obterP :: [Integer] -> Int -> (Integer, Int)
+obterP primos x = if ( atual `mod` e /= 1 ) 
+                then (atual, x)
+                else obterP primos (x + 1)
+    where atual = primos !! x
+          e = 3
+
+
+
+-- retorna o par (p,q)
+obterPQ :: [Integer] -> (Integer, Integer)
+obterPQ primos = (p, fst $ obterP primos (pi+1))
+    where (p,pi) = obterP primos 0
+
+
+
+-- teste de primitividade de Rabin Miller
+primoMillerRabin :: StdGen -> Integer -> Bool
+primoMillerRabin gen n = if n `mod` 2 == 0 
+                           then False
+                           else resultado 
+    where resultado = foldl (\acc x -> (acc == True && x == True)) True rlist
+          rlist = map (iteracaoMillerRabin s n ) (listaMillerRabin n 30 gen)
+          (_,s) = formaMillerRabin n
+
+formaMillerRabin :: (Integral a) => a -> (a, a)
+formaMillerRabin n = (d, fromIntegral s)
+    where listaFatores = iterate (`div` 2) (pred n)
+          s = length $ takeWhile (\x -> x `mod` 2 == 0) $ listaFatores
+          d = listaFatores !! s
+
+listaMillerRabin :: Integer -> Int -> StdGen -> [Integer]
+listaMillerRabin _ 0 _ = []
+listaMillerRabin n k gen  = 
+    let (d,s)         = formaMillerRabin n
+        (a,newGen)    = randomR (2, (n-2)) gen
+        testeRabin    = moduloExp a d n
+        resultado     = if ( testeRabin == 1 || testeRabin == (n-1) ) 
+                        then listaMillerRabin n (k-1) newGen
+                        else testeRabin : listaMillerRabin n (k-1) newGen  
+    in resultado
+
+iteracaoMillerRabin :: (Integral a, Integral b) => b-> a -> a -> Bool
+iteracaoMillerRabin s n x
+    | k == 1 = False
+    | s == 1 = False 
+    | k == (n-1) = True
+    | otherwise  = iteracaoMillerRabin (s-1) n k
+    where k = moduloExp x 2 n
+
+
+
+-- codifica um texto em uma lista de inteiros
+codificarTextoInt :: String -> [Integer]
+codificarTextoInt texto = map (blocoInt 128 0) $ dividirBloco 64 $ textoBloco texto 
+
+
+
+-- decodifica um texto codificado em uma lista de inteiros
+decodificarIntTexto :: [Integer] -> String 
+decodificarIntTexto texto = unwords textoDividido 
+    where textoDividido = map (blocoTexto . intBloco) texto
+
+
+
+-- funcao para calcular b^e `mod` x
+moduloExp :: (Integral a, Integral b) => a -> b -> a -> a
+moduloExp b e x 
+    | e == 0 = 1
+    | (odd e) = ( b * (moduloExp (k) (e `div` 2) x)) `mod` x 
+    | otherwise = (moduloExp (k) (e `div` 2) x)
+    where k = (b*b) `mod` x
+
+
+
+-- substitui uma lista de caracteres por uma lista dos respectivos valores ASCII
+textoBloco :: [Char] -> [Int]
+textoBloco texto = map (ord) texto
+
+
+
+-- substitui os valores inteiros por seu respectivo caracter ASCII
+blocoTexto :: [Int] -> [Char]
+blocoTexto texto = map (chr) texto 
+
+
+
+-- dividir os valores ASCII em blocos 
+dividirBloco :: Int -> [Int] -> [[Int]]
+dividirBloco _ [] = []
+dividirBloco len bloco = inicioBloco : dividirBloco len caudaBloco
+    where inicioBloco = take len bloco
+          caudaBloco = drop len bloco 
+
+
+
+-- cria uma base inteira
+blocoInt :: Integer -> Integer -> [Int] -> Integer
+blocoInt _ _ [] = 0
+blocoInt base expoente (x:xs) = (fromIntegral (x) * base^expoente) + (blocoInt base (expoente+1) xs)
+
+
+
+-- cria uma lista de digitos inteiros na base 128
+intBloco :: Integer -> [Int]
+intBloco 0 = []
+intBloco n = fromIntegral (n `mod` 128) : intBloco (n `div` 128)
